da3c50568dc95058eb34fb8bb094d87ae6043e0f
prajwalg42 <prajwalgaikwad42@gmail.com>
2024-02-22 13:18:01 +0530

    changes done in settings

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database1',  # database name in RDS is written here
+        'NAME': 'djangoblog',  # database name in RDS is written here
         'USER': 'admin',  # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         # database endpoint is written here
-        'HOST': 'aws-capstone-rds.cukd79ofsohr.us-east-1.rds.amazonaws.com',
+        'HOST': 'database-1.cdgsq8uskaok.ap-south-1.rds.amazonaws.com',
         'PORT': '3306'  # database port number is written here
     }
 }
@@ -144,9 +144,9 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'enesblog'  # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'djangoblog'  # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = "us-east-1"  # please enter your s3 region
+AWS_S3_REGION_NAME = "ap-south-1"  # please enter your s3 region
 AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
